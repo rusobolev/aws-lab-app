c2fbb793178f888d63e4ab9c2ace8da3d3bdc6e3
Aleksandr Sobolev <aleksandr_sobolev2@epam.com>
2025-03-21 19:11:39 +0400

    added support for IMDSv2

--- server.js
+++ server.js
@@ -4,16 +4,34 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/metadata', async (req, res) => {
+async function getMetadata(path) {
     try {
-        const region = await axios.get('http://169.254.169.254/latest/meta-data/placement/region');
-        const az = await axios.get('http://169.254.169.254/latest/meta-data/placement/availability-zone');
-        
-        res.json({
-            region: region.data,
-            availabilityZone: az.data
+        const tokenResponse = await axios.put(
+            'http://169.254.169.254/latest/api/token',
+            null,
+            { headers: { 'X-aws-ec2-metadata-token-ttl-seconds': '21600' } }
+        );
+
+        const token = tokenResponse.data;
+
+        const response = await axios.get(`http://169.254.169.254/latest/meta-data/${path}`, {
+            headers: { 'X-aws-ec2-metadata-token': token }
         });
+
+        return response.data;
     } catch (error) {
+        console.error('Metadata fetch error:', error.message);
+        return null;
+    }
+}
+
+app.get('/metadata', async (req, res) => {
+    const region = await getMetadata('placement/region');
+    const az = await getMetadata('placement/availability-zone');
+
+    if (region && az) {
+        res.json({ region, availabilityZone: az });
+    } else {
         res.status(500).json({ error: 'Unable to fetch metadata' });
     }
 });
